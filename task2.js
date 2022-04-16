3d294eee994f5629bf4a1b641549d1e63a8b66fc
asherginting <asherginting3@gmail.com>
2022-04-16 15:54:08 +0700

    fix: task2

--- task2.js
+++ task2.js
@@ -1,8 +1,8 @@
 const mtk = 80
-const bahasaIndonesia = 90
-const bahasaInggris = 89
+const bIndo = 90
+const bIng = 89
 const ipa = 69
-const nilaiAkhir = ((mtk + bahasaIndonesia + bahasaInggris + ipa) / 4)
+const nilaiAkhir = ((mtk + bIndo + bIng + ipa) / 4)
 
 let grade
 
@@ -20,40 +20,5 @@ if (nilaiAkhir >= 90) {
 
 console.log( "Rata-rata: " + nilaiAkhir)
 console.log ( "Grade: " + grade)
-
-// const mtk = 80
-// const bahasaIndonesia = 90
-// const bahasaInggris = 89
-// const ipa = 69
-
-// let rataRata = 0
-// let grade = ""
-
-
-// if (mtk !== null && mtk !== undefined) {
-//     if(bahasaIndonesia !== null && bahasaIndonesia !== undefined) {
-//         if(bahasaInggris !== null && bahasaInggris !== undefined) {
-//             if(ipa !== null && ipa !== undefined) {
-//                 const rataRata = mtk + bahasaIndonesia + bahasaInggris + ipa / 4
-//                 if (rataRata >= 90 && rataRata <= 100) {
-//                     grade = "A"
-//                 } else if (rataRata >= 80) {
-//                     grade = "B"
-//                 } else if (rataRata >= 70) {
-//                     grade = "C"
-//                 } else if (rataRata >= 60) {
-//                     grade = "D"
-//                 } else if (rataRata >= 0) {
-//                     grade = "E"
-//                 } else {
-//                     grade = "None"
-//                 }
-//             }
-//         }
-//     }
-// }
-
-// console.log( "Rata Rata : " + rataRata)
-// console.log( "Grade : " + grade)
 
 
