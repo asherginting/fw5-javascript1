9fcefe7b8859d3852fe4553a049124b28e515df5
asherginting <asherginting3@gmail.com>
2022-04-16 15:53:54 +0700

    fix: task1

--- task1.js
+++ task1.js
@@ -1,18 +1,18 @@
 const biodata = {
     name: "Asher Azriel Ginting",
     age: 22,
-    hobbies: ["Musik", "Volley", "Nonton Film"],
+    hobbies: ["Musik", "Volley", "Watching Film"],
     isMaried: false,
     schoolList: 
     {
-        namaSekolah: "Universitas Putera Batam",
-        tahunMasuk: 2017,
-        tahunkeluar: 2021
+        nameSchool: "University Of Putera Batam",
+        yearIn: 2017,
+        yearOut: 2021
     },
     skills: 
     {
-        namaSkill: "HTML dan CSS",
-        levelSkill: "Menengah"
+        nameSkill: "Web and Android Development",
+        levelSkill: "Beginner"
     },
     interestInCoding: true,
 }
